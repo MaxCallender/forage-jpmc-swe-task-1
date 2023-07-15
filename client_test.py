ce29d4a183e01cde4f758a6c3c8ccc48c46c7eb4
Max <maxcallender@gmail.com>
2023-07-15 21:36:56 +0100

    Added assertions and unitests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+      for quote in qutoes:
+          self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +17,30 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+      for quote in qutoes:
+          self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
+    def test_getRatio_priceBIsZero(self):
+        price_a = 100
+        price_b = 0
+        #testing output when price_b = 0 (should return nothing)
+        self.assertIsNone(getRatio(price_a, price_b))
+    
+    def test_getRatio_priceAIsZero(self):
+        price_a = 0
+        price_b = 100
+        #testing for same when price_a = 0
+        self.assertEqual(getRatio(price_a, price_b), 0)
 
+    
+    def test_getRatio_bothPricesAreZero(self):
+        price_a = 0
+        price_b = 0
+        #testing 0/0
+        self.assertIsNone(getRatio(price_a, price_b))
+        self.assertIsNone(getRatio(price_b, price_a))
 
 
 if __name__ == '__main__':
